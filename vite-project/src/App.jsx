38fdbe621a6bb0865d5e9bf6676cc7d514fb81f3
kiselkakakaka <klim200387@gmail.com>
2023-09-20 15:36:29 +0300

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// Задание 7
+// function App() {
+// 	return <div>
+// 		Я - правда!
+//     <p className="It get">Эй, йоу, детка, ты не знаешь</p>
+//     <p className="worse">Как плохо это может быть.</p>
+// 	</div>;
+// }
+
+//Задание 8
+// function App() {
+// 	return	<ul>
+//     <li>1</li>
+//     <li>2</li>
+//     <li>3</li>
+//     <li>4</li>
+//     <li>5</li>
+//     <li>6</li>
+//     <li>7</li>
+//     <li>8</li>
+//     <li>9</li>
+//     <li>10</li>
+// 	</ul>;
+// }
+
+//Задание 9
+// function App() {
+// 	return (
+// 		<ul>
+// 			<li>text1</li>
+// 			<li>text2</li>
+// 			<li>text3</li>
+// 		</ul>
+//     )
+// }
+
+
+//Задание 10
+// function App() {
+// 	return (
+//   <> 
+//   <ul>
+// 		<li>text1</li>
+// 		<li>text2</li>
+// 		<li>text3</li>
+// 	</ul>
+  
+// 	<ul> 
+// 		<li>text1</li>
+// 		<li>text2</li>
+// 		<li>text3</li>
+// 	</ul>
+  
+//   </>
+//   );
+// }
+
+//Задание 11
+// function App() {
+// 	return <input/>;
+// }
+
+//Задание 12
+// function App() {
+// 	return (
+//   <>
+//   <input/><input/><input/>
+//   </>
+//   );
+// }
+
+//Задание 13
+// function App() {
+// 	const str1 = 'text1';
+// 	const str2 = 'text1';
+	
+// 	return <div>
+// 		<p>{str1}</p>
+// 		<p>{str2}</p>
+// 	</div>;
+// }
+
+export default App
