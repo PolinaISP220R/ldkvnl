39b0d57bd18767bdd0310852f8d93b032f2ba184
PolinaISP220R <103931096+PolinaISP220R@users.noreply.github.com>
2023-09-22 15:40:33 +0300

    App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 // Задание 7
-// function App() {
+//export default function App() {
 // 	return <div>
 // 		Я - правда!
 //     <p className="It get">Эй, йоу, детка, ты не знаешь</p>
@@ -12,7 +12,7 @@ import './App.css'
 // }
 
 //Задание 8
-// function App() {
+//export default function App() {
 // 	return	<ul>
 //     <li>1</li>
 //     <li>2</li>
@@ -28,7 +28,7 @@ import './App.css'
 // }
 
 //Задание 9
-// function App() {
+//export default function App() {
 // 	return (
 // 		<ul>
 // 			<li>text1</li>
@@ -40,7 +40,7 @@ import './App.css'
 
 
 //Задание 10
-// function App() {
+//export default function App() {
 // 	return (
 //   <> 
 //   <ul>
@@ -60,12 +60,12 @@ import './App.css'
 // }
 
 //Задание 11
-// function App() {
+//export default function App() {
 // 	return <input/>;
 // }
 
 //Задание 12
-// function App() {
+//export default function App() {
 // 	return (
 //   <>
 //   <input/><input/><input/>
@@ -74,14 +74,139 @@ import './App.css'
 // }
 
 //Задание 13
-// function App() {
+// export default function App() {
 // 	const str1 = 'text1';
-// 	const str2 = 'text1';
+//  	const str2 = 'text1';
 	
 // 	return <div>
 // 		<p>{str1}</p>
 // 		<p>{str2}</p>
+//  	</div>;
+//  }
+
+//Задание 14
+//export default function App() {
+    
+// 	const name = 'user';
+// 	const age  = '30';
+	
+// 	return <div>
+//         <>
+// 		name: {name},
+// 		age:  {age}
+//     </>
+// 	</div>;
+// }
+// export default App
+
+//Задание 15
+// export default function App () {
+// 	const arr = [1, 2, 3, 4, 5];
+
+//     return <div>
+// 		<ul>
+//         <li>{arr[0]}</li>
+//         <li>{arr[1]}</li>
+//         <li>{arr[2]}</li>
+//         <li>{arr[3]}</li>
+//         <li>{arr[4]}</li>
+//         </ul>
 // 	</div>;
 // }
 
-export default App
+//Задание 16
+// export default function App() {
+// 	const obj = {name: 'john', surname: 'smit'};
+// 	    <p>
+//           name:<span>{obj.name}</span> <br>
+//           surname:<span>{obj.surname}</span>
+//         </p>
+// }
+	
+//Задание 17
+//  export default function  App() {
+// 	const attr = 'block';
+	
+// 	return <div> 
+//         id={attr}text
+//     </div>
+// }
+
+// Задание 19
+// export default function App() {
+//     const li1 = <li>text1</li>;
+// 	const li2 = <li>text2</li>;
+// 	const li3 = <li>text3</li>;
+
+//     return(
+//         <ul>
+// 	<li>{li1}</li>
+// 	<li>{li2}</li>
+// 	<li>{li3}</li>
+// </ul>
+//     );
+// }
+
+//Задание 20
+// export default function App() {
+//     const items = <>
+//     <li>text1</li><li>text2</li><li>text3</li>
+//     </>;
+	
+// 	return <ul>
+// 		<li>{items}</li>
+// 	</ul>
+// }
+
+//Задание 23
+// export default function App() {
+//         return <div>
+//             <input /><br/>
+//             <input />
+//         </div>;
+//     }
+
+//Задание 24.1
+// export default function App() {
+// 	return <div>
+// 		<ui>
+//             <li>1</li>
+//             <li>11</li>
+//             <li>111</li>
+//             <li>1111</li>
+//             <li>11111</li>
+//             <li>111111</li>
+//             <li>1111111</li>
+//             <li>11111111</li>
+//             <li>111111111</li>
+//             <li>1111111111</li>
+
+//         </ui>
+// 	</div>;
+// }
+
+//Задание 24.2
+// export default function App() {
+//     return <table>
+// 		<tbody>
+// 			<tr>
+// 				<td>1</td>
+// 				<td>2</td>
+//                 <td>3</td>
+                
+// 			</tr>
+// 			<tr>
+// 				<td>4</td>
+// 				<td>5</td>
+//                 <td>6</td>
+// 			</tr>
+//             <tr>
+// 				<td>7</td>
+// 				<td>8</td>
+//                 <td>9</td>
+// 			</tr>
+// 		</tbody>
+// 	</table>;
+// }
+
+//Задание 25 1:result:9; 2:result:john smit; 3:result:2
